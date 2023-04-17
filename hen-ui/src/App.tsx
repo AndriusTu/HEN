1caa5d51e0634a806e5c4c025162bbd3311e7e9b
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2023-04-17 23:27:47 +0300

    Add routing

--- hen-ui/src/App.tsx
+++ hen-ui/src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 
 function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
