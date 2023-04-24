439be5b3e97d050437245602d1977ec4f1bddec2
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2023-04-24 21:25:15 +0300

    Create delivery methods tab

--- hen-ui/src/pages/createParcel/components/DeliveryMethodsTab.tsx
+++ hen-ui/src/pages/createParcel/components/DeliveryMethodsTab.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import { Text, List } from '../../../components';
-import DeliveryMethodCard from './DeliveryMethodCard';
-import deliveryMethodsMock from '../../../mocks/deliveryMethodsMock';
+import React from "react";
+import { Text, List } from "../../../components";
+import DeliveryMethodCard from "./DeliveryMethodCard";
+import deliveryMethodsMock from "../../../mocks/deliveryMethodsMock";
 
 function DeliveryMethodsTab() {
-  const [activeCard, setActiveCard] = React.useState<string | undefined>(
-    undefined,
-  );
-
   return (
     <div className="flex flex-col items-center justify-start mb-[26px] p-[31px] sm:px-5 w-[99%] md:w-full">
       <div className="flex flex-col gap-[21px] items-start justify-start mb-[261px] w-[98%] md:w-full">
@@ -19,21 +15,14 @@ function DeliveryMethodsTab() {
           Choose your delivery type:
         </Text>
         <List
-          className="sm:flex-col flex-row md:gap-10 gap-10 grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center w-full"
+          className="sm:flex-col flex-row md:gap-10 gap-[60px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center w-full"
           orientation="horizontal"
         >
-          {deliveryMethodsMock.map((deliveryMethod, index) => (
-            <DeliveryMethodCard
-              key={index}
-              {...deliveryMethod}
-              isActive={activeCard === deliveryMethod.deliveryType}
-              onClick={() => setActiveCard(deliveryMethod.deliveryType)}
-            />
-          ))}
+          <DeliveryMethodCard {...deliveryMethodsMock[0]}/>
         </List>
       </div>
     </div>
-  );
+  )
 }
 
 export default DeliveryMethodsTab;
