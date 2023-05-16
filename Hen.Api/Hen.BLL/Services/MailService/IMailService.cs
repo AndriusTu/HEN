eaf60bf2b8caa5b79c225d7f0f309a92b712f886
Arnasst <59619111+Arnasst@users.noreply.github.com>
2023-05-16 20:49:10 +0300

    Added mail service

---
+++ Hen.Api/Hen.BLL/Services/MailService/IMailService.cs
@@ -0,0 +1,8 @@
+﻿using Hen.DAL.Enums;
+
+namespace Hen.BLL.Services.MailService;
+public interface IMailService
+{
+    public Task SendStatusUpdate(string receiverMail, string receiverName, Guid parcelId, DeliveryStatus newStatus);
+}
+
