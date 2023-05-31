f8f972832c59a55a7bf576255258c87ce3dd6c1d
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2023-05-31 17:36:17 +0300

    Add phone number and email validation to forms

--- hen-ui/src/utils/stringUtils.ts
+++ hen-ui/src/utils/stringUtils.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(word) {
   return word.toLowerCase().replace(/^\w/, (c) => c.toUpperCase());
 }
+
+export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+export const lithuanianPhoneRegex = /^(\+3706|86)(\d{7})$/;
